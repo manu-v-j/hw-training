6ce41a0642a21c55df1ba4a38b127bd905df95b1
Manu V J <manuvj1234@gmail.com>
2025-05-02 16:55:53 +0530

    commit the extracted information from the website

--- 2025-05-02/sortiment/crawler.py
+++ 2025-05-02/sortiment/crawler.py
@@ -10,14 +10,21 @@ def crawler(url):
     if unique_id:
         match = re.search(r'\d+', unique_id)
         unique_id = match.group() if match else None
+    product_name=selector.xpath("//span[@class='base']//text()").get().strip()
+    price=selector.xpath("//strong[@class='pricefield__price']/@content").get()
+    image_url=selector.xpath("//img[@class='gallery-placeholder__image']/@src").getall()
+    currency=selector.xpath("//span[contains(@class,'price-currency')]/text()").get()
+    reviews=selector.xpath("//a[@class='action view']/span[1]//text()").get()
+    breadcrumps=[b.strip() for b in selector.xpath("//div[@class='breadcrumbs']/ul/li//text()").getall() if b.strip()]
+    description=" ".join(selector.xpath("//div[@class='col-left']/p[2]//text()").getall()).strip()
 
     return {
-        "unique_id": unique_id,
-        "product_name": selector.xpath("//span[@class='base']//text()").get().strip(),
-        "price": selector.xpath("//strong[@class='pricefield__price']/@content").get(),
-        "image_url" : selector.xpath("//img[@class='gallery-placeholder__image']/@src").getall(),
-        "currency":selector.xpath("//span[contains(@class,'price-currency')]/text()").get(),
-        "reviews":selector.xpath("//a[@class='action view']/span[1]//text()").get(),
-        "breadcrumps": [b.strip() for b in selector.xpath("//div[@class='breadcrumbs']/ul/li//text()").getall() if b.strip()],
-        "description": " ".join(selector.xpath("//div[@class='col-left']/p[2]//text()").getall()).strip()
+        "Unique_id": unique_id,
+        "Product_name":product_name ,
+        "Price": price,
+        "Image_url" : image_url,
+        "Currency":currency,
+        "Reviews":reviews,
+        "Breadcrumps":breadcrumps ,
+        "description": description
     }
