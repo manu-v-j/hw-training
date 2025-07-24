cc2a2af728e003c86600ba72e89403feff2cd0d3
Manu V J <manuvj1234@gmail.com>
2025-07-24 09:49:20 +0530

    commit the extracted information

--- 2025-07-23/2xlhome_scraper.py
+++ 2025-07-23/2xlhome_scraper.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 import json
 
-df = pd.read_csv('/home/user/Hashwave/2025-07-23/2xlhome.csv')
+df = pd.read_csv('/home/user/Hashwave/2025-07-23/2xlhome .csv')
 df.columns = df.columns.str.strip().str.replace('-', '_').str.replace(' ', '_')
 
 df['selling_price'] = df['regular_price'].str.extract(r'Special\s*Price\s*AED\s*([\d,]+)', flags=re.IGNORECASE)
@@ -22,7 +22,7 @@ df_final = df[['url', 'product_name', 'selling_price', 'regular_price_clean', 'b
                'promotion_description', 'image', 'product_description']]
 
 df_final.rename(columns={'regular_price_clean': 'regular_price'}, inplace=True)
-
-output_file = '/home/user/Hashwave/2025-07-23/2xlhome_clean.csv'
+df_final = df_final.head(50)
+output_file = '/home/user/Hashwave/2025-07-23/2xlhome_20250723.csv'
 df_final.to_csv(output_file, index=False)
 
