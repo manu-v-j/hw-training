9b504f7bd6f8b1cc5d194fc15464c4564337aafa
Manu V J <manuvj1234@gmail.com>
2025-07-24 10:36:06 +0530

    commit the extracted information from the website

--- 2025-07-23/2xlhome_scraper.py
+++ 2025-07-23/2xlhome_scraper.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import re
-import json
 
 df = pd.read_csv('/home/user/Hashwave/2025-07-23/2xlhome .csv')
 df.columns = df.columns.str.strip().str.replace('-', '_').str.replace(' ', '_')
@@ -11,18 +10,22 @@ df['selling_price'] = df['selling_price'].str.replace(',', '', regex=True).astyp
 df['regular_price_clean'] = df['regular_price'].str.extract(r'Regular\s*Price\s*AED\s*([\d,]+)', flags=re.IGNORECASE)
 df['regular_price_clean'] = df['regular_price_clean'].str.replace(',', '', regex=True).astype(float)
 
-df['promotion_description'] = df['premotion_description']
+df['breadcrumbs'] = df['breadcrumbs'].fillna('').apply(lambda x: x.split(' ', 1))
+df.rename(columns={
+    'premotion_description': 'promotion_description',
+    'product_liink_href': 'url',  
+    'image_src': 'image'
+}, inplace=True)
 
-df['url'] = df['product_liink_href']
-df['image'] = df['image_src']
-
-df.drop(columns=['regular_price', 'web_scraper_order', 'web_scraper_start_url', 'product_liink', 'premotion_description'], inplace=True)
+df.drop(columns=['regular_price', 'web_scraper_order', 'web_scraper_start_url', 'product_liink'], errors='ignore', inplace=True)
 
 df_final = df[['url', 'product_name', 'selling_price', 'regular_price_clean', 'breadcrumbs',
-               'promotion_description', 'image', 'product_description']]
+               'promotion_description', 'image', 'product_description']].copy()
 
 df_final.rename(columns={'regular_price_clean': 'regular_price'}, inplace=True)
+
 df_final = df_final.head(50)
-output_file = '/home/user/Hashwave/2025-07-23/2xlhome_20250723.csv'
+
+output_file = '/home/user/Hashwave/2025-07-23/2xlhome_clean.csv'
 df_final.to_csv(output_file, index=False)
 
