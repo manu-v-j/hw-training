05780629327d8f2a6d034be0e2635fb842a137e0
Manu V J <manuvj1234@gmail.com>
2025-08-01 14:32:07 +0530

    commit the extracted information

--- 2025-08-01/robertsbrothers_webscrper.py
+++ 2025-08-01/robertsbrothers_webscrper.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import re
+import json
 
 df = pd.read_csv('/home/user/Hashwave/2025-08-01/robertsbrothers_site.csv')
-df = df.drop(columns=['web-scraper-order', 'web-scraper-start-url', 'agent_link','website'])
-df = df.rename(columns={'agent_link-href': 'profile_url','website-href':'website'})
+df = df.drop(columns=['web-scraper-order','website'])
+df = df.rename(columns={'web-scraper-start-url': 'profile_url','website-href':'website'})
 df['name'] = df['name'].str.split('\n').str[0].str.strip()
 df['description'] = df['description'].str.replace('\n', ' ', regex=False).str.strip()
 df['description'] = (df['description'].str.replace('\n', ' ', regex=False).str.replace(r'\s+', ' ', regex=True).str.strip()                               
@@ -13,5 +14,12 @@ df['country']='US'
 df_final = df[['title', 'address', 'profile_url', 'description', 'name',
                'website', 'agent_phone_numbers', 'country']].copy()
 
-print(df_final['title'])
-df=df_final.to_json("ewm.json", orient="records", lines=True)
+data = df_final.to_json("robertsbrothers.json", orient="records", lines=True)
+
+result = []
+with open(data, 'r', encoding='utf-8') as f:
+    for line in f:
+        data.append(json.loads(line))
+
+# Print as a list of dicts
+print(result)
