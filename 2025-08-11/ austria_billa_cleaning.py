4350338815fb35de3e83643a6792e056b4c6d647
Manu V J <manuvj1234@gmail.com>
2025-08-11 15:57:40 +0530

    commit the extracted infromation from the website

--- 2025-08-11/ austria_billa_cleaning.py
+++ 2025-08-11/ austria_billa_cleaning.py
@@ -3,10 +3,15 @@ import re
 df = pd.read_csv("/home/toshiba/Hashwave/2025-08-11/DataHut_AT_Billa_PriceExtractions_20250806.csv")
 df["regular_price"] = df["regular_price"].replace("", pd.NA) 
 df["regular_price"] = df["regular_price"].fillna(df["selling_price"])
+
 df['regular_price'] = df['regular_price'].astype(float).apply(lambda x: f"{x:.2f}")
 df['selling_price'] = df['selling_price'].astype(float).apply(lambda x: f"{x:.2f}")
-df['price_per_unit'] = df['price_per_unit'].str.replace(',', '', regex=False)
 
+print(df[['regular_price', 'selling_price']].tail())
+
+# Check the data types
+print(df[['regular_price', 'selling_price']].dtypes)
+df['price_per_unit'] = df['price_per_unit'].str.replace(',', '', regex=False)
 df["breadcrumb"] = df["breadcrumb"].replace("", pd.NA)
 empty_breadcrumb_count = df["breadcrumb"].isna().sum()
 
@@ -27,4 +32,4 @@ df.loc[df['site_shown_uom'].isin(keywords_three), 'grammage_unit'] = 'stuck'
 df['competitor_name']=df['competitor_name'].replace('billaa','billa',regex=False)
 df['currency']=df['currency'].replace('eur','EUR',regex=False)
 
-df.to_csv("/home/toshiba/Hashwave/2025-08-11/DataHut_AT_Billa_PriceExtractions_20250811.csv", index=False)
+# df.to_csv("/home/toshiba/Hashwave/2025-08-11/DataHut_AT_Billa_PriceExtractions_20250811.csv", index=False)
